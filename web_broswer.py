a8409889aa4829b047157eb854b454636dc355b7
Light Bekete <167067896+LightBekete@users.noreply.github.com>
2025-05-07 00:39:36 +0200

    added from PyQt5.QtCore import Qt and QGuiApplication.setAttribute(Qt.AA_ShareOpenGLContexts) to handle graohics possibly

--- web_broswer.py
+++ web_broswer.py
@@ -1,6 +1,6 @@
 # This Python file loads the PyQtWeb_browser.qml and instatiates the QtWebEngine 
 import sys
-
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QGuiApplication
 from PyQt5.QtQml import QQmlApplicationEngine
 from PyQt5.QtWebEngine import QtWebEngine
@@ -8,6 +8,8 @@ from PyQt5.QtWebEngine import QtWebEngine
 
 
 if __name__ == "__main__":
+    # possibly to handler graphics
+    QGuiApplication.setAttribute(Qt.AA_ShareOpenGLContexts)
     app = QGuiApplication(sys.argv)
     
     # this line below initialises the QtWebEngine which the the PyQtWeb_browser.qml will use to load
