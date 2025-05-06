5e99ae01acc47b7832fc58fcca6af80d5cc3d7b7
Light bekete <bsc-com-ne-07-21@unima.ac.mw>
2025-05-06 10:46:34 +0200

    main.qml file renamed to PyQtWeb_broswer.qml and main.py renamed to web_browser

--- web_broswer.py
+++ web_broswer.py
@@ -9,7 +9,7 @@ from PyQt5.QtQml import QQmlApplicationEngine
 if __name__ == "__main__":
     app = QGuiApplication(sys.argv)
     engine = QQmlApplicationEngine()
-    engine.load("main.qml")
+    engine.load("PyQtWeb_browser.qml")
     if not engine.rootObjects():
         sys.exit(-1)
     sys.exit(app.exec())
