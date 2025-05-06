a27aa568e89cefcff8926ac22131cb786f57e085
Light bekete <bsc-com-ne-07-21@unima.ac.mw>
2025-05-06 12:30:56 +0200

    the web_browser.py  file has been modified ,the Qtwebengine has been imported and initialiseed successfully. and the Ptqtweb_browser.qml has been modified too the actual user interaction with the qtwebengine has been achieved from the importation of the qtwebengine in the .qml file and uses the WebengineView qml element from the qtwebengine to render html documents. for now i have staticlly assigned the http://www.google.com url to the webengineview for testing sake

--- PyQtWeb_browser.qml
+++ PyQtWeb_browser.qml
@@ -1,10 +1,20 @@
+// this is the qml file which contains and uses the QtWebEngine to render
+// html documents from specified http urls
+
 import QtQuick 2.15
 import QtQuick.Controls 2.15
+import QtWebEngine 1.10
 
 ApplicationWindow {
     width: screen.width
     height: screen.height
     visible: true
     title: qsTr("Qt Web Broswer")
+
+    WebEngineView {
+            id: webView
+            anchors.fill: parent
+            url: "https://www.google.com"
+        }
 
 }
--- web_broswer.py
+++ web_broswer.py
@@ -1,13 +1,19 @@
-# This Python file uses the following encoding: utf-8
+# This Python file loads the PyQtWeb_browser.qml and instatiates the QtWebEngine 
 import sys
 
 from PyQt5.QtGui import QGuiApplication
 from PyQt5.QtQml import QQmlApplicationEngine
+from PyQt5.QtWebEngine import QtWebEngine
 
 
 
 if __name__ == "__main__":
     app = QGuiApplication(sys.argv)
+    
+    # this line below initialises the QtWebEngine which the the PyQtWeb_browser.qml will use to load
+    # the html documents
+    QtWebEngine.initialize()
+    
     engine = QQmlApplicationEngine()
     engine.load("PyQtWeb_browser.qml")
     if not engine.rootObjects():
